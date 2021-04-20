efa08d30a0bfd77f4442a88822ba3ee9f828088c
moussaddak <meddeb.moussaddak@gmail.com>
2021-04-20 13:15:40 +0000

    executable

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -12,13 +12,3 @@ def canUnlockAll(boxes):
         return True
     else:
         return False
-
-boxes = [[1], [2], [3], [4], []]
-print(canUnlockAll(boxes))
-
-boxes = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
-print(canUnlockAll(boxes))
-
-boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
-print(canUnlockAll(boxes))
-
