63dca681b7ba625441db3f6857b9acb1512c7b13
Moussaddak Meddeb <52888741+Moussaddak@users.noreply.github.com>
2021-04-21 14:14:26 +0100

    Update 0-lockboxes.py

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -11,7 +11,7 @@ def canUnlockAll(boxes):
     keys = [0]
     for index, box in enumerate(boxes):
         for key in box:
-            if index in box and index != len(boxes) - 1:
+            if index in box and index <= len(boxes) - 1:
                 return False
             elif key not in keys:
                 keys.append(key)
