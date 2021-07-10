34709145a5ee20885c180b54c5dad75fa1b5e381
Moussaddak Meddeb <52888741+Moussaddak@users.noreply.github.com>
2021-07-10 17:21:53 +0100

    New Solution

---
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+UTF-8 Validation of data set
+"""
+
+
+def validUTF8(data):
+    """
+        :type data: List[int]
+        :rtype: bool
+    """
+    
+    count = 0
+    for num in data:
+        tmp = 1 << 7
+        if count == 0:
+            while tmp & num:
+                count += 1
+                tmp >>= 1
+            if count > 4:
+                return False
+        elif num >> 6 != 2:
+            return False
+        if count:
+            count -= 1
+    return count == 0
