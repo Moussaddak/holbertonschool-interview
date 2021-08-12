ba12547dae8c486b2821253fc12f5082f9d8d7cf
Moussaddak <meddeb.moussaddak@gmail.com>
2021-08-12 18:31:27 +0100

    up

---
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+
+const request = require('request');
+const id = process.argv[2];
+const url = `https://swapi-api.hbtn.io/api/films/${id}/`;
+
+request(url, async function (error, response, body) {
+    if (error) { console.log(error); }
+
+    const movie = JSON.parse(body);
+    const chars = movie.characters;
+
+    for (let i = 0; i < chars.length; i++) {
+        const name = await new Promise(function (resolve, reject) {
+            request(chars[i], 'utf-8', function (error, response, body) {
+                if (error) { reject(error); }
+                resolve(JSON.parse(body).name);
+            });
+        });
+        console.log(name);
+    }
+
+});
